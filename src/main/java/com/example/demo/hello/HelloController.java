3f4631d4d42062a6bfafbfc0aaf422b5ffe8d14e
Harsh123A <pushpendra.kumar@netcreativemind.com>
2024-04-24 12:22:34 +0530

    some changes

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -16,45 +16,46 @@ import com.example.demo.Service.StudentServiceImpl;
 @Controller
 public class HelloController {
 
+	@Autowired
+	private StudentServiceImpl employeeService;
 
-    @Autowired
-    private StudentServiceImpl employeeService;
-	/*
-	 * @GetMapping("/") public String index() { return "navbar"; }
-	 */
+	@GetMapping("/")
+	public String index() {
+		return "navbar";
+	}
 
-    @GetMapping("/")
-    public String viewHomePage(Model model) {
-        model.addAttribute("listEmployees", employeeService.getAllEmployees());
-    	System.out.println("working new path");
-        return "index";
-    }
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		System.out.println("working new path");
+		return "index";
+	}
 
-    @GetMapping("/showNewEmployeeForm")
-    public String showNewEmployeeForm(Model model) {
-        // create model attribute to bind form data
-        Student employee = new Student();
-        model.addAttribute("employee", employee);
-        return "new_employee";
-    }
+	@GetMapping("/showNewEmployeeForm")
+	public String showNewEmployeeForm(Model model) {
+		// create model attribute to bind form data
+		Student employee = new Student();
+		model.addAttribute("employee", employee);
+		return "new_employee";
+	}
 
-    @PostMapping("/saveEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Student employee) {
-        employeeService.saveEmployee(employee);
-        return "redirect:/";
-    }
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Student employee) {
+		employeeService.saveEmployee(employee);
+		return "redirect:/";
+	}
 
-    @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
-    	Student employee = employeeService.getEmployeeById(id);
-        model.addAttribute("employee", employee);
-        return "update_employee";
-    }
+	@GetMapping("/showFormForUpdate/{id}")
+	public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+		Student employee = employeeService.getEmployeeById(id);
+		model.addAttribute("employee", employee);
+		return "update_employee";
+	}
 
-    @GetMapping("/deleteEmployee/{id}")
-    public String deleteEmployee(@PathVariable(value = "id") long id) {
+	@GetMapping("/deleteEmployee/{id}")
+	public String deleteEmployee(@PathVariable(value = "id") long id) {
 
-        this.employeeService.deleteEmployeeById(id);
-        return "redirect:/";
-    }
+		this.employeeService.deleteEmployeeById(id);
+		return "redirect:/";
+	}
 }
